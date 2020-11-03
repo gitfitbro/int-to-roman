cb729ba1ca569dbe3f4e579336dc2a771626c82b
Eddie Gutierrez <eddygutierrez175@gmail.com>
2020-11-02 16:59:14 -0800

    Added 2 functions that find the numbers first place integer and the other function converts that integer to a roman numeral

--- app.js
+++ app.js
@@ -4,5 +4,38 @@
  * @return {string}
  */
 const intToRoman = function(num) {
-
+  const onesPlace = findOnesPlaceInt(num);
+  const onesPlaceRoman = convertOnesToRoman(onesPlace);
 };
+
+// Find the number in the ones place given any number 
+const findOnesPlaceInt = function(num) {
+  const integer = num % 10;
+  return integer; 
+};
+
+const convertOnesToRoman = function(num) {
+/*
+ // TODO: return Roman numeral 4
+    if (num === 4) {
+        return 'IV';
+    }
+ // TODO: return Roman numeral 9 
+    if (num === 9) {
+        return 'IX';
+    }
+*/
+    let romanNumeral = '';
+    switch(num) {
+      case 4:
+        romanNumeral = 'IV';
+        break;
+      case 9: 
+        romanNumeral = 'IX';
+        break;
+      default: 
+        romanNumeral = 'Coming Soon';
+    }
+}
+
+console.log(`${findOnesPlaceInt(8002)}`);
